4b5c3ad2d9dbe4251ff26a5c5fe47c32532981d1
Tevania <terence.renard@gmail.com>
2024-01-13 02:16:03 +0100

    commentaire

--- config/connexion.php
+++ config/connexion.php
@@ -2,7 +2,7 @@
 // Connexion à la base de données.
 function connect($config){
     try{
-        $db = new PDO('mysql:host='.$config['serveur'].';dbname='.$config['bd'], $config['login'], $config['mdp'], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+        $db = new PDO('mysql:host='.$config['serveur'].';dbname='.$config['bd'], $config['login'], $config['mdp'], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')); // On utilise PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' pour forcer tous les champs à s'afficher en utf8 quoiqu'il arrive. 
     }
  catch(Exception $e){
     $db = NULL;
